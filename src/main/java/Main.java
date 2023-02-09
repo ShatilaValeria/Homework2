52c08be02694a18529330df0ce39ee94d26a90a5
ValeriaShatila <123481670+ValeriaShatila@users.noreply.github.com>
2023-02-09 14:23:51 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
         controller.start();
-        System.out.println("123");
     }
 }
